ec41b298953c9cfd7f3488c364c6b7db0dde2d43
klashxx <klashxx@gmail.com>
2016-08-06 12:38:43 +0200

    code refactoring

--- lib/stackoverflow.go
+++ lib/stackoverflow.go
@@ -6,41 +6,49 @@ import (
 	"regexp"
 )
 
+func GetTags(userid int, key string, offline bool) string {
+
+	if offline {
+		Trace.Printf("Not avaliable offline")
+		return ""
+	}
+
+	tags := new(SOTopTags)
+	tagstr := ""
+
+	url := fmt.Sprintf("%s/%s%s", SOApiURL, fmt.Sprintf(SOUserTags, userid), key)
+
+	if err := StreamHTTP(url, tags, true); err != nil {
+		Trace.Printf("No info for: %d\n", userid)
+		return ""
+	}
+
+	if len(tags.Items) == 0 {
+		Trace.Printf("No info for: %d\n", userid)
+		return ""
+	}
+
+	for _, tag := range tags.Items {
+		tagstr = fmt.Sprintf("%s<li>%s</li>", tagstr, tag.TagName)
+	}
+
+	return tagstr
+}
+
 func GetUserInfo(users *SOUsers, min int, location *regexp.Regexp, counter *int, limit int, ranks *Ranks, term bool, offline bool, key string) bool {
 
-	var url string
-	var tagstr string
-	tags := new(SOTopTags)
-
 	for _, user := range users.Items {
-		tagstr = ""
 		Trace.Printf("Procesing user: %d\n", user.UserID)
 
 		if user.Reputation < min {
 			return false
 		}
+
 		if location.MatchString(user.Location) {
 			*counter += 1
 			if *counter == 1 && term {
 				Info.Println("User data:")
 				Info.Printf("%4s %-30s %6s %s\n", "Rank", "Name", "Rep", "Location")
-			}
-
-			if !offline {
-				url = fmt.Sprintf("%s/%s%s", SOApiURL, fmt.Sprintf(SOUserTags, user.UserID), key)
-
-				if err := StreamHTTP(url, tags, true); err != nil {
-					Trace.Printf("No info for: %d\n", user.UserID)
-				} else {
-					if len(tags.Items) > 0 {
-						for _, tag := range tags.Items {
-							tagstr = fmt.Sprintf("%s<li>%s</li>", tagstr, tag.TagName)
-						}
-					} else {
-						Trace.Printf("No info for: %d\n", user.UserID)
-					}
-				}
-
 			}
 
 			s := SOUserRank{Rank: *counter,
@@ -51,7 +59,7 @@ func GetUserInfo(users *SOUsers, min int, location *regexp.Regexp, counter *int,
 				WebsiteURL:   user.WebsiteURL,
 				Link:         user.Link,
 				ProfileImage: user.ProfileImage,
-				TopTags:      tagstr}
+				TopTags:      GetTags(user.UserID, key, offline)}
 
 			*ranks = append(*ranks, s)
 
