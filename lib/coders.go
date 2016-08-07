3a90818b8e125950cbcd9e2a24fa1912e2a077d0
klashxx <klashxx@gmail.com>
2016-08-07 20:31:50 +0200

    coders grouped

--- lib/coders.go
+++ lib/coders.go
@@ -6,11 +6,14 @@ import (
 	"io"
 )
 
+func JSONDecoder(r io.Reader, data interface{}) error {
+	return json.NewDecoder(r).Decode(data)
+}
+
 func JSONEncoder(data interface{}) (buf io.ReadWriter, err error) {
 
 	buf = new(bytes.Buffer)
-	err = json.NewEncoder(buf).Encode(data)
-	if err != nil {
+	if err := json.NewEncoder(buf).Encode(data); err != nil {
 		return nil, err
 	}
 	return buf, nil
